c03881dcd348765242fee282b97469ac64e45cc2
Somesh Bhosale <someshbhosale2@gmail.com>
2024-04-24 12:11:46 +0530

    Synchronisation project deploy

--- main.py
+++ main.py
@@ -94,4 +94,4 @@ def retrieve_data():
         return jsonify({'error': 'Error retrieving hospital data from the database'})
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
